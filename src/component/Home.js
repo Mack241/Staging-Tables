1f0ce1e9342f83e4c5680c85fef473124199d36c
Mayukh Dasgupta <mayukhdasgupta18@gmail.com>
2022-01-23 23:28:06 +0530

    Staging Tables commit 4

--- src/component/Home.js
+++ src/component/Home.js
@@ -14,11 +14,19 @@ const Home = () => {
 
     return (
         <div>
+            <div className='background'>
+                <div className="title">
+                    <h1>Staging Tables</h1>
+                    <p className="description">Stage your tables in a smooth and flawless manner without having the hassle of coding them manually. Click on <b><i>Create Table</i></b> to continue</p>
+                </div>
+            </div>
             <Button
                 className='create-table-button'
                 value={modal}
                 style={{ marginTop: '60px' }}
-                onClick={handleModal}>Create Table</Button>
+                onClick={handleModal}>
+                    Create Table
+            </Button>
             {modal
                 ?
                 <ModalComponent modal={modal} />
