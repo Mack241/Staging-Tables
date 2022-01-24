c767f2e57175badce678021a84e4d3aa38dfdd22
Mayukh Dasgupta <34231653+Mack241@users.noreply.github.com>
2022-01-24 10:35:45 +0530

    Home.js updated

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Button } from '@material-ui/core'
 import './Home.css'
-import Table from './Table'
 import ModalComponent from './ModalComponent'
 
 const Home = () => {
@@ -33,7 +32,6 @@ const Home = () => {
                 :
                 <></>
             }
-            <Table />
         </div>
     )
 }
